59e110e1ad30d908369659414eb8e9c92b9dd20d
Breinss <breandanw@upcmail.nl>
2015-09-07 19:12:39 +0200

    Project Boilerplate + setup done

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	int playerState;
+	Vector3 pos;
+	// Use this for initialization
+	void Start () {
+		playerState = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += pos;
+
+		if(Input.GetKeyUp("space")){
+            if (playerState == 1)
+            {
+                playerState = 2;
+                print("PlayerState Set to 2");
+            }
+            else
+            {
+                playerState = 1;
+                print("PlayerState Set to 1");
+            }
+            
+        }
+
+		switch (playerState) {
+		case 1:
+                pos.y = 0.01f;
+			break;
+
+		case 2:
+                pos.y = -0.01f;
+                break;
+		}
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+    }
+}
